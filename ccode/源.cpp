a381720bf5c52e977db67025333db1f9e0e38d82
wujh <3130568528@qq.com>
2026-06-19 22:07:49 +0800

    成员变量与成员函数分开存储

--- ccode/源.cpp
+++ ccode/源.cpp
@@ -2,23 +2,29 @@
 using namespace std;
 
 
+//成员变量和成员函数分开存储
 class Person {
-public:
 
-	static void func() {
-		cout << "func" << endl;
+	int m_A;
+
+	static int m_B; // 静态成员变量不占用对象内存
+
+	void func() {
 	}
-
 };
 
-
+int Person::m_B = 0; // 静态成员变量需要在类外进行初始化
 
 void test01() {
 	Person p;
-	p.func();
-	Person::func();
+
+	cout << sizeof(p) << endl; // 1
 }
 
+void test02() {
+	Person p;
+	cout<<sizeof(p)<<endl;
+}
 
 
 int main() {
