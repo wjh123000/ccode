bf7127626b537dd5b3338a6e48c8d84b57da8a70
wujh <3130568528@qq.com>
2026-06-21 22:37:20 +0800

    readfile2

--- ccode/源.cpp
+++ ccode/源.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 #include<fstream>
-
+#include<string>
 
 
 void test01() {
@@ -27,17 +27,53 @@ void test02() {
 	//}
 
 	// 方式2：按行读取
-	char buf2[1024] = { 0 };
-	while (ifs.getline(buf2, sizeof(buf2))) {
-		cout << buf2 << endl;
+	//char buf2[1024] = { 0 };
+	//while (ifs.getline(buf2, sizeof(buf2))) {
+	//	cout << buf2 << endl;
+	//}
+
+	// 方式3：按字符串读取
+	//string buf;
+	//while (getline(ifs, buf)) {
+	//	cout << buf << endl;
+	//}
+
+	// 方式4：按字符读取
+	char c;
+	while ((c=ifs.get()) != EOF) {
+		cout << c;
 	}
 
 	ifs.close();
 }
 
+class Person {
+public:
+	char m_Name[64];
+	int m_Age;
+};
+// 二进制文件写入
+void test03() {
+	ofstream ofs("person.txt", ios::out | ios::binary);
+	Person p={"张三", 20};
+	ofs.write((const char*)&p, sizeof(Person));
+	ofs.close();
+}
+// 二进制文件读取
+void test04() {
+	ifstream ifs("person.txt", ios::in | ios::binary);
+	if (!ifs.is_open()) {
+		cout << "文件打开失败！" << endl;
+		return;
+	}
+	Person p;
+	ifs.read((char*)&p, sizeof(Person));
+	cout << p.m_Name << " " << p.m_Age << endl;
+	ifs.close();
+}
+
 int main() {
-	test01();
-	test02();
+	test04();
 
 	return 0;
 }
